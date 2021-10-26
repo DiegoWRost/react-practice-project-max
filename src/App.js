5643b0726101d226b5e5dc7382f741ff0fe483fc
DiegoWRost <diego.wr@gmail.com>
2021-10-26 14:24:56 -0600

    105. Working with Portals

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from "react";
+import React, { useState } from "react";
 
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
@@ -16,10 +16,10 @@ function App() {
   };
 
   return (
-    <Fragment>
+    <React.Fragment>
       <AddUser onAddUser={addUserHandler}></AddUser>
       <UsersList users={usersList}></UsersList>
-    </Fragment>
+    </React.Fragment>
   );
 }
 
